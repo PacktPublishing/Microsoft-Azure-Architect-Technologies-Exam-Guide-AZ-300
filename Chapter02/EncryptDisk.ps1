615c3d028032e2da4d54809056dc77a74a2be741
Sjoukje Zaal <SZaal@outlook.com>
2019-08-09 17:06:32 +0200

    Added Ch2

---
+++ Chapter02/EncryptDisk.ps1
@@ -0,0 +1,21 @@
+﻿Connect-AzAccount
+
+Select-AzSubscription -SubscriptionId "********-****-****-****-***********"
+
+#Set some paramaters
+$ResourceGroupName = 'PacktVMResourceGroup'
+$vmName = 'PacktVM1'
+$KeyVaultName = 'PacktEncryptionVault'
+
+#Retrieve the Key Vault
+$KeyVault = Get-AzKeyVault -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName
+$diskEncryptionKeyVaultUrl = $KeyVault.VaultUri
+$KeyVaultResourceId = $KeyVault.ResourceId
+
+#Encrypt disk
+Set-AzVMDiskEncryptionExtension `
+    -ResourceGroupName $ResourceGroupName `
+    -VMName $vmName `
+    -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl `
+    -DiskEncryptionKeyVaultId $KeyVaultResourceId
+

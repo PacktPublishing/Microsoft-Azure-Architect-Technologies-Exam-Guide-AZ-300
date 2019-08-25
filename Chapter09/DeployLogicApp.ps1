9d2f12292558375b4b618d676684e60b980eb399
Sjoukje Zaal <SZaal@outlook.com>
2019-08-25 09:16:55 +0200

    Added

---
+++ Chapter09/DeployLogicApp.ps1
@@ -0,0 +1,14 @@
+﻿# First connect to your Azure Account.
+Connect-AzAccount
+
+# Select the subscription to deploy the App to.
+Select-AzSubscription -SubscriptionId "********-****-****-****-***********"
+
+#Create a resource group
+New-AzResourceGroup -Name PacktLogicAppResourceGroup -Location EastUS
+
+#Deploy the template inside your Azure subscription 
+New-AzRmResourceGroupDeployment `
+    -Name PacktDeployment `
+    -ResourceGroupName PacktLogicAppResourceGroup `
+    -TemplateFile c:\MyTemplates\template.json

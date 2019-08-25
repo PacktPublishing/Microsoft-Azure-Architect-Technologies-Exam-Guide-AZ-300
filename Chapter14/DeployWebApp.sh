ec848ecf7e90f61a5b490b3dcafd9807540dc57a
Sjoukje Zaal <SZaal@outlook.com>
2019-08-25 18:16:29 +0200

    Create DeployWebApp.sh

---
+++ Chapter14/DeployWebApp.sh
@@ -0,0 +1,18 @@
+gitrepo="https://github.com/PacktPublishing/Microsoft-Azure-Architect-Technologies-Exam-Guide-AZ-300/Chapter14/PacktPubToDoAPI"
+webappname="PacktPubToDoAPI"
+
+# Create a resource group.
+az group create --location eastus --name PacktWebAppResourceGroup
+
+# Create an App Service plan in the FREE tier.
+az appservice plan create --name $webappname --resource-group PacktWebAppResourceGroup --sku FREE
+
+# Create a web app.
+az webapp create --name $webappname --resource-group PacktWebAppResourceGroup --plan $webappname
+
+# Deploy code from a public GitHub repository. 
+az webapp deployment source config --name $webappname --resource-group PacktWebAppResourceGroup \
+--repo-url $gitrepo --branch master --manual-integration
+
+# Copy the result of the following command into a browser to see the web app.
+echo http://$webappname.azurewebsites.net

19893532dacc73764c6fe36d7fea1dd2ae0e1c36
Sjoukje Zaal <SZaal@outlook.com>
2019-09-04 21:11:19 +0200

    Updated

--- Chapter14/DeployWebApp.sh
+++ Chapter14/DeployWebApp.sh
@@ -13,6 +13,3 @@ az webapp create --name $webappname --resource-group PacktWebAppResourceGroup --
 # Deploy code from a public GitHub repository. 
 az webapp deployment source config --name $webappname --resource-group PacktWebAppResourceGroup \
 --repo-url $gitrepo --branch master --manual-integration
-
-# Copy the result of the following command into a browser to see the web app.
-echo http://$webappname.azurewebsites.net

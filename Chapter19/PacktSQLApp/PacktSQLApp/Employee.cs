cd8e1adac0bb11f7348c11ef3450b5c5d2d47289
Sjoukje Zaal <SZaal@outlook.com>
2019-10-13 17:21:27 +0200

    Added ch19

---
+++ Chapter19/PacktSQLApp/PacktSQLApp/Employee.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace PacktSQLApp
+{
+
+    public class Employee
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? HireDate { get; set; }
+        //public Address[] Addresses { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
